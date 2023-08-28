318cc818606d65b45d1da517aab86934a185a23d
Jmike8385e <142643946+Jmike8385e@users.noreply.github.com>
2023-08-28 05:04:17 +0000

    changed all my to jer

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 //'require' is the method
 //'http'is a code library that is pre-written in node.js, single or doubel quotes both work
 
-let myhttp = require('http');
+let jerhttp = require('http');
 
 // use dot notation in javascript to access an object and its methods (commands) and properties
 // createServer() takes some arguments to make it provide a useful http server
@@ -14,26 +14,26 @@ let myhttp = require('http');
 //using the variable declared above 'myhttp' which then connects to a retrieved variable 'createServer'
 //evewrything that starts with 'my' is user created, others are built in
 
-let myserver = myhttp.createServer(
-  function( myrequest, myresponse ) {
-    console.log( myrequest.url );
+let jerserver = jerhttp.createServer(
+  function( jerrequest, jerresponse ) {
+    console.log( jerrequest.url );
 
-    let mytext;
-    if ( myrequest.url === "/luke" ) {
-      mytext = "I ain't yo daddy";
+    let jertext;
+    if ( jerrequest.url === "/luke" ) {
+      jertext = "I ain't yo daddy";
     } else {
-      mytext = "No idea what you just said. Try again please.";
+      jertext = "No idea what you just said. Try again please.";
     }
     // writeHead() creates an http response header, including the status code (200 OK), the content type
-    myresponse.writeHead( 200, { "Content-Type": "text/plain" } );
+    jerresponse.writeHead( 200, { "Content-Type": "text/plain" } );
 
-    myresponse.end( mytext );
+    jerresponse.end( jertext );
   }
 );
 
 // ask http to start listening on a tcp port for incoming http requests
 
 // listen() takes 2 args: 1: tcp port #, 2: string of the ip address to listen (0.0.0.0)
-myserver.listen(8080, "0.0.0.0");
+jerserver.listen(8080, "0.0.0.0");
 
 console.log("Let's get this server started!");
